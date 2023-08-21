b082fa6ddb73de9d71dd29627130852684316cbc
Hira Tariq <i.hiratariq@gmail.com>
2023-08-21 15:54:31 +0500

    final keyword use in class lib/model/news_api_model.dart

--- lib/model/news_api_model.dart
+++ lib/model/news_api_model.dart
@@ -1,95 +1,162 @@
-
-class NewsApiModel {
-  String? status;
-  int? totalResults;
-  List<Articles>? articles;
-
-  NewsApiModel({this.status, this.totalResults, this.articles});
-
-  NewsApiModel.fromJson(Map<String, dynamic> json) {
-    status = json['status'];
-    totalResults = json['totalResults'];
-    if (json['articles'] != null) {
-      articles = <Articles>[];
-      json['articles'].forEach((v) {
-        articles!.add(Articles.fromJson(v));
-      });
-    }
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['status'] = status;
-    data['totalResults'] = totalResults;
-    if (articles != null) {
-      data['articles'] = articles!.map((v) => v.toJson()).toList();
-    }
-    return data;
-  }
-}
+import 'dart:convert';
 
 class Articles {
-  Source? source;
-  String? author;
-  String? title;
-  String? description;
-  String? url;
-  String? urlToImage;
-  String? publishedAt;
-  String? content;
+  final Source? source;
+  final String author;
+  final String title;
+  final String description;
+  final String? url;
+  final String? urlToImage;
+  final String publishedAt;
+  final String content;
+  Articles({
+    this.source,
+    required this.author,
+    required this.title,
+    required this.description,
+    this.url,
+    this.urlToImage,
+    required this.publishedAt,
+    required this.content,
+  });
 
-  Articles(
-      {this.source,
-      this.author,
-      this.title,
-      this.description,
-      this.url,
-      this.urlToImage,
-      this.publishedAt,
-      this.content});
 
-  Articles.fromJson(Map<String, dynamic> json) {
-    source = json['source'] != null ? Source.fromJson(json['source']) : null;
-    author = json['author'];
-    title = json['title'];
-    description = json['description'];
-    url = json['url'];
-    urlToImage = json['urlToImage'];
-    publishedAt = json['publishedAt'];
-    content = json['content'];
+
+  Articles copyWith({
+    Source? source,
+    String? author,
+    String? title,
+    String? description,
+    String? url,
+    String? urlToImage,
+    String? publishedAt,
+    String? content,
+  }) {
+    return Articles(
+      source: source ?? this.source,
+      author: author ?? this.author,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      url: url ?? this.url,
+      urlToImage: urlToImage ?? this.urlToImage,
+      publishedAt: publishedAt ?? this.publishedAt,
+      content: content ?? this.content,
+    );
   }
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    if (source != null) {
-      data['source'] = source!.toJson();
-    }
-    data['author'] = author;
-    data['title'] = title;
-    data['description'] = description;
-    data['url'] = url;
-    data['urlToImage'] = urlToImage;
-    data['publishedAt'] = publishedAt;
-    data['content'] = content;
-    return data;
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'source': source?.toMap(),
+      'author': author,
+      'title': title,
+      'description': description,
+      'url': url,
+      'urlToImage': urlToImage,
+      'publishedAt': publishedAt,
+      'content': content,
+    };
+  }
+
+  factory Articles.fromMap(Map<String, dynamic> map) {
+    return Articles(
+      source: map['source'] != null ? Source.fromMap(map['source'] as Map<String,dynamic>) : null,
+      author: map['author'] as String,
+      title: map['title'] as String,
+      description: map['description'] as String,
+      url: map['url'] != null ? map['url'] as String : null,
+      urlToImage: map['urlToImage'] != null ? map['urlToImage'] as String : null,
+      publishedAt: map['publishedAt'] as String,
+      content: map['content'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Articles.fromJson(String source) => Articles.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Articles(source: $source, author: $author, title: $title, description: $description, url: $url, urlToImage: $urlToImage, publishedAt: $publishedAt, content: $content)';
+  }
+
+  @override
+  bool operator ==(covariant Articles other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.source == source &&
+      other.author == author &&
+      other.title == title &&
+      other.description == description &&
+      other.url == url &&
+      other.urlToImage == urlToImage &&
+      other.publishedAt == publishedAt &&
+      other.content == content;
+  }
+
+  @override
+  int get hashCode {
+    return source.hashCode ^
+      author.hashCode ^
+      title.hashCode ^
+      description.hashCode ^
+      url.hashCode ^
+      urlToImage.hashCode ^
+      publishedAt.hashCode ^
+      content.hashCode;
   }
 }
 
 class Source {
-  String? id;
-  String? name;
+ final String? id;
+ final String? name;
+  Source({
+    this.id,
+    this.name,
+  });
 
-  Source({this.id, this.name});
+ 
 
-  Source.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
+  Source copyWith({
+    String? id,
+    String? name,
+  }) {
+    return Source(
+      id: id ?? this.id,
+      name: name ?? this.name,
+    );
   }
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['id'] = id;
-    data['name'] = name;
-    return data;
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+    };
   }
+
+  factory Source.fromMap(Map<String, dynamic> map) {
+    return Source(
+      id: map['id'] != null ? map['id'] as String : null,
+      name: map['name'] != null ? map['name'] as String : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Source.fromJson(String source) => Source.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() => 'Source(id: $id, name: $name)';
+
+  @override
+  bool operator ==(covariant Source other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.id == id &&
+      other.name == name;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ name.hashCode;
 }
